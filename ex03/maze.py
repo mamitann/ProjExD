f9dda6a056be2ac8fed72b952978b67e8d1160fd
araya <c0a2100900@edu.teu.ac.jp>
2022-12-06 14:29:59 +0900

    練習3－4

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,4 +11,6 @@ if __name__ == "__main__":
     kokaton = tk.PhotoImage(file="fig/8.png")
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image=kokaton, tag="kokaton")
+
+    key = ""
     root.mainloop()
