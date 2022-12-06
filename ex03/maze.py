1f58099f871d7dc08ff512920d5d67160f6430e9
araya <c0a2100900@edu.teu.ac.jp>
2022-12-06 16:59:46 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import maze_maker as mm
+#import random 
 
 def key_down(event):
     global key
@@ -9,8 +10,21 @@ def key_up(evnet):
     global key
     key = ""
 
+def print_clear():
+    root2 = tk.Tk()
+    canvas2 = tk.Canvas(root2, width=500, height=500, bg="white")
+    canvas2.pack()
+    gameclear = tk.PhotoImage(file="fig/text_gameclear_j.png", master = root2)
+    canvas2.create_image(250, 250, image=gameclear, tag="game_clear")
+
+    root2.mainloop()
+
 def main_proc():
-    global cx, cy, mx, my
+    global cx, cy, mx, my, gameclear
+
+    if (mx == 13 and my == 7):
+        print_clear()
+
     if key == "Up": my -= 1
     if key == "Down": my += 1
     if key == "Left": mx -= 1
@@ -40,13 +54,18 @@ if __name__ == "__main__":
     
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50
+    sx, sy = 1, 1
+    gx, gy = 13, 7
+
+    gameclear = None
     
+    canvas.create_oval(sx*100, sy*100, sx*100+100, sy*100+100, fill="green")  #スタートの追加
+    canvas.create_oval(gx*100, gy*100, gx*100+100, gy*100+100, fill="red")  #ゴールの追加
     canvas.create_image(cx, cy, image=kokaton, tag="kokaton")
 
     key = ""
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
     main_proc()
-
 
     root.mainloop()
