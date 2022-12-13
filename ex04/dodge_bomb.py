f5bb8a9e07cba1202854c38bc6a527b84d8fd98c
araya <c0a2100900@edu.teu.ac.jp>
2022-12-13 14:17:33 +0900

    練習4-2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,10 +3,21 @@ import sys
 
 
 def main():
+    clock = pg.time.Clock()
+
     pg.display.set_caption("逃げろこうかとん") 
     scrn_sfc = pg.display.set_mode((1600, 900))
+    pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
+    pgbg_rct = pgbg_sfc.get_rect()
 
-    pg_bg_sfc = pg.image.load("fig/pg_bg.jpg")
+    #練習2
+    while True:
+        scrn_sfc.blit(pgbg_sfc, pgbg_rct)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        pg.display.update() 
+        clock.tick(1000)
 
 if __name__ == "__main__":
     pg.init()
